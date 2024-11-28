08700c3d14296b5be5dda1e1075c31245826d0da
RandomPigYT <vinecompilation864@gmail.com>
2024-11-29 00:39:44 +0530

    Minor changes

--- src/main.c
+++ src/main.c
@@ -1,10 +1,45 @@
 #include <stdio.h>
+#include <string.h>
 #include "libinetsocket.h"
 
-int main(int argc, char **argv) {
-  (void)argc;
-  (void)argv;
+#define SET_ERR_STR(str)                             \
+  do {                                               \
+    snprintf(err_str, sizeof(err_str), "%s", (str)); \
+    goto Error;                                      \
+                                                     \
+  } while (0)
 
-  printf("Hello world!\n");
+int server(void) {
+  printf("Server!\n");
+
   return 0;
+}
+
+int client(void) {
+  printf("Client!\n");
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  char err_str[256];
+
+  if (argc <= 1) {
+    SET_ERR_STR("server [client/server]");
+  }
+
+  if (strcmp("server", argv[1]) == 0) {
+    server();
+
+  } else if (strcmp("client", argv[1]) == 0) {
+    client();
+
+  } else {
+    SET_ERR_STR("server [client/server]");
+  }
+
+  return 0;
+
+Error:
+  fprintf(stderr, "Bad usage: %s\n", err_str);
+  return 1;
 }
